84a4d6693494355383c55a3eb658758f5a8eb1b7
LouisSloot <louissloot2705@gmail.com>
2025-06-06 00:51:37 -0500

    feat: gitignore updates and config file progress

--- configs/tsn-imagenet-finetuned-cfg.py
+++ configs/tsn-imagenet-finetuned-cfg.py
@@ -13,3 +13,49 @@ default_hooks = dict(
     sampler_seed=dict(type='DistSamplerSeedHook'),
     sync_buffers=dict(type='SyncBuffersHook'),
     timer=dict(type='IterTimerHook'))
+default_scope = 'mmaction'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+file_client_args = dict(io_backend='disk')
+load_from = None
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=20)
+model = dict(
+    type='Recognizer3D',
+    backbone=dict(
+        type='SwinTransformer3D',
+        arch='tiny',  
+        pretrained=None,
+        pretrained2d=True,
+        patch_size=(2, 4, 4),
+        in_channels=3,
+        window_size=(8, 7, 7),
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.1,
+        act_cfg=dict(type='GELU'),
+        norm_cfg=dict(type='LN'),
+        patch_norm=True,
+        frozen_stages=-1,
+        with_cp=False,
+        out_indices=(3,),
+        out_after_downsample=False),
+    cls_head=dict(
+        type='I3DHead',
+        in_channels=768,
+        num_classes=3, # adjust as action classes change
+        spatial_type='avg',
+        dropout_ratio=0.5),
+    data_preprocessor=dict(
+        type='ActionDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        format_shape='NCTHW'),
+    train_cfg=None,
+    test_cfg=None
+)
