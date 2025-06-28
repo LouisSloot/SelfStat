879e1a945cc6aecc4279c38d11cc223a8c16219e
Louis Sloot <lsloot@andrew.cmu.edu>
2025-06-28 11:54:34 -0500

    Upload of initial finetuning file

---
+++ act_reg_model/finetune.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from torchvision.models.video import r3d_18
+import cv2
+import numpy as np
+from pathlib import Path
+import os
+from tqdm import tqdm
+import argparse
+import json
+import random
+
+
