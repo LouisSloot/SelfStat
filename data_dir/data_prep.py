49d5eb63f6a1da83527bcf09067aa0297b580872
Louis Sloot <lsloot@andrew.cmu.edu>
2025-07-01 18:07:44 -0500

    building out VideoDataset class and clarified some wording

--- data_dir/data_prep.py
+++ data_dir/data_prep.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 from torchvision.models.video import r3d_18
+from torchvision.io import read_video
 import cv2
 import numpy as np
 from pathlib import Path
@@ -13,10 +14,67 @@ import argparse
 import json
 import random
 
+# I use "label" to mean "dribble", "layup", or "shoot"
+# I use "class" to mean 0, 1, or 2
 
 ### Define a pytorch Dataset class for MultiSubjects videos
 class VideoDataset(Dataset):
-    def __init__(self, annotation_file, data_dir, clip_frames = 63,
+    def __init__(self, annotation_file, data_root, split, clip_frames = 63,
                  transform = None):
-        self.data_dir = data_dir
+        self.data_root = data_root
         self.clip_frames = clip_frames # 63 frames = ~2.5s at 25 fps
+        self.transform = transform
+        self.split = split # "train", "val", or "test"
+        self.annotations = []
+
+        # build out list of self.annotations
+        f = open(annotation_file, 'r')
+        for line in f: # "{video_file} [0/1/2]"
+            line = line.strip()
+            if line:
+                parts = line.split()
+                video_name = parts[0]
+                video_class = int(parts[1])
+                self.annotations.append((video_name, video_class))
+        f.close()
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        video_name, video_class = self.annotations[index]
+        video_label = video_label_from_name(video_name)
+        sub_dir = self.split + "_" + video_label
+        full_path = f'{self.data_root}/{sub_dir}/{video_name}'
+
+        video, _, _ = read_video(full_path, pts_unit = "sec", 
+                                 output_format = "CTHW")
+        
+        # trim/pad video to the correct number of frames
+        if video.shape[1] > self.clip_frames:
+            start = random.randint(0, video.shape[1] - self.clip_frames)
+            video = video[:, start:start + self.clip_frames, :, :]
+
+        elif video.shape[1] < self.clip_frames:
+            pad = self.frames_per_clip - video.shape[1]
+            video = torch.nn.functional.pad(video, (0, 0, 0, 0, 0, pad))
+        
+        if self.transform: # will always be true (in this script at least)
+            video = self.transform(video)
+        
+        return video, video_class
+
+    def augment_video(self, video):
+        pass
+
+    
+def video_label_from_name(video_name):
+    # video_name should be as appears in annotation file
+    label_map = {
+        'd': "dribble",
+        'p': "layup",
+        's': "shoot"
+    }
+    parts = video_name.split("_")
+    video_label = label_map[parts[1]]
+    return video_label
--- data_dir/organize_data.py
+++ data_dir/organize_data.py
@@ -1,7 +1,7 @@
 import os
 
 ### Script to restructure the MultiSubjects data splits in a clearer way
-class_map = {
+label_map = {
     'd': "dribble",
     'p': "layup",
     's': "shoot"
@@ -9,18 +9,18 @@ class_map = {
 
 def restructure():
     for split in ["train", "val", "test"]:
-        for data_class in ["dribble", "shoot", "layup"]:
-            dir_name = split + "_" + data_class
+        for data_label in ["dribble", "shoot", "layup"]:
+            dir_name = split + "_" + data_label
             os.mkdir(f"./train_val_test/{split}/{dir_name}")
         split_folder_path = f"./train_val_test/{split}"
         for video_name in os.listdir(split_folder_path):
             split_name = video_name.split("_") # "xxx_[d/s/p]_x.mp4 [0/1/2]"
-            class_char = split_name[1]
-            if class_char not in set(['d', 'p', 's']):
+            label_char = split_name[1]
+            if label_char not in set(['d', 'p', 's']):
                 continue
-            video_class = class_map[class_char]
+            video_label = label_map[label_char]
             src = f"./train_val_test/{split}/{video_name}"
-            new_parent_folder = split + "_" + video_class
+            new_parent_folder = split + "_" + video_label
             dst = f"./train_val_test/{split}/{new_parent_folder}/{video_name}"
             os.rename(src = src, dst = dst)
 
