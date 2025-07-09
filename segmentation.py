a8d64bf8bc851f945c1e17190164c7cf8fad6872
Louis Sloot <lsloot@andrew.cmu.edu>
2025-07-08 21:33:09 -0500

    Small edits. Next step is to get a better ball identification model since native yolo options aren't consistent enough.

--- segmentation.py
+++ segmentation.py
@@ -3,11 +3,16 @@ from ultralytics import YOLO
 player_team_map = {1: "A", 2: "A", 3: "A", 
                    4: "B", 5: "B", 6: "B"}
 
+def ball_found(ball_box):
+    """ Determines whether or not there is a bounding box to be drawn for 
+        the basektball. """
+    return bool(ball_box.xyxy.tolist())
+
 def find_nearest_player(ball_box, person_boxes):
     """ Used to find the player currently in possession of the basketball.
         Returns None if no player appears to have possession (loose ball).
         Current naive approach relies solely on IoU. """
-    if not ball_box: return None
+    if not ball_found(ball_box): return None
     best_IOU = 0
     best_person = None
     for person_box in person_boxes:
@@ -18,6 +23,7 @@ def find_nearest_player(ball_box, person_boxes):
     return best_person
 
 def findIOU(xyxy1, xyxy2):
+    """ Standard Intersection over Union function. """
     left1, top1, right1, btm1 = xyxy1
     left2, top2, right2, btm2 = xyxy2
     
@@ -42,24 +48,24 @@ class FrameRecord():
                        boxes.cls]
         ball_mask = [result.names[cls.item()] == "sports ball" for cls in 
                      boxes.cls]
+        
         self.ball_box = boxes[ball_mask]
         self.person_boxes = boxes[person_mask]
 
-        self.frame_idx = FrameRecord.frame_idx
-        FrameRecord.frame_idx += 1
-        # self.ball_box, self.ball_in_frame = (ball_box, True if ball_box else 
-                                            #  None, False)
         self.player_possession = find_nearest_player(self.ball_box, 
                                                      self.person_boxes)
         self.team_possession = player_team_map.get(self.player_possession, None)
 
+        self.frame_idx = FrameRecord.frame_idx
+        FrameRecord.frame_idx += 1
+
+records = []
+
 model = YOLO("./yolo11m.pt")
 
-src = "./data_dir/raw_games/game_1.mp4"
+src = "./data_dir/raw_games/game_2.mp4"
 
-results = model.predict(src, stream = True, conf = 0.4)
-
-keep_classes = {"sports ball", "person"}
+results = model.predict(src, stream = True, conf = 0.4, verbose = False)
 
 for frame_num, result in enumerate(results):
 
@@ -67,6 +73,8 @@ for frame_num, result in enumerate(results):
 
     record = FrameRecord(result)
 
-    result.boxes = record.player_possession # determine which box(es) to draw
+    records.append(record)
+
+    result.boxes = record.ball_box # determine which box(es) to draw
 
     result.show()  # display to screen
