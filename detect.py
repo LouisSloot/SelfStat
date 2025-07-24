2526d64a2b9ffb8c17c3297ce78b4801ddb02391
Louis Sloot <lsloot@andrew.cmu.edu>
2025-07-24 12:56:43 -0500

    Fixed detect method to work with videos rather than images.

--- detect.py
+++ detect.py
@@ -5,8 +5,7 @@ class YOLODetector:
         self.model = YOLO(model_path)
         self.conf = conf
 
-    def detect(self, frame):
-        results = self.model.predict(source = frame, stream = False,
+    def detect(self, src):
+        results = self.model.predict(source = src, stream = True,
                                           conf = self.conf)
-        boxes = results[0]
-        return boxes
+        return results
