8140b7dd0177cccd4188159459b6774d95dbd1cd
Louis Sloot <lsloot@andrew.cmu.edu>
2025-08-20 22:36:42 -0400

    Correcting some import usages after interfaces were changed.

--- track_players.py
+++ track_players.py
@@ -5,13 +5,12 @@ from create_replay import create_replay
 
 def track_players():
     src = "./data_dir/raw_games/game_2_good_start.MP4"
-    FRAME_NUM = 1 # will eventually be input by the user
     num_players = 6 # testing with videos of 3v3 games
 
     model_path = "./YOLO/yolo12n.pt"
     detector = YOLODetector(model_path, conf = 0.5)
 
-    sv_ids, crops, selected_boxes = supervised_label(detector, src, FRAME_NUM)
+    sv_ids, crops, selected_boxes = supervised_label(detector, src)
     id_manager = IdentityManager(num_players, sv_ids)
     id_manager.build_embedding_refs(crops)
     id_manager.build_last_pos(selected_boxes)
